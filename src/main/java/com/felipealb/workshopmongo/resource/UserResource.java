6daa2ca456a5830da7e4f47881fb200580ac35be
felipe ross <kamigamink@gmail.com>
2026-02-17 18:00:33 -0300

    Database instantiation operation

--- src/main/java/com/felipealb/workshopmongo/resource/UserResource.java
+++ src/main/java/com/felipealb/workshopmongo/resource/UserResource.java
@@ -1,13 +1,12 @@
 package com.felipealb.workshopmongo.resource;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felipealb.workshopmongo.domain.User;
@@ -20,7 +19,7 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public  ResponseEntity< List<User>> findAll(){
 		List<User> list = service.findAll();	
 		return ResponseEntity.ok().body(list);
