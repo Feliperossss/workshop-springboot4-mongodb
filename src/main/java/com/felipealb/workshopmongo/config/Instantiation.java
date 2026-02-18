4222868f3550368fabfdec43375ef9f0d761da03
felipe ross <kamigamink@gmail.com>
2026-02-18 16:17:45 -0300

    Author Data Projection with DTO

--- src/main/java/com/felipealb/workshopmongo/config/Instantiation.java
+++ src/main/java/com/felipealb/workshopmongo/config/Instantiation.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.felipealb.workshopmongo.DTO.AuthorDTO;
 import com.felipealb.workshopmongo.domain.Post;
 import com.felipealb.workshopmongo.domain.User;
 import com.felipealb.workshopmongo.repository.PostRepository;
@@ -36,8 +37,8 @@ public class Instantiation implements CommandLineRunner {
 		User bob = new User(null, "Bob Grey", "bob@gmail.com"); 
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
-		Post post1 = new Post(null, Instant.now(), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null,Instant.now(), "Bom dia", "Acordei feliz hoje!", maria);
+		Post post1 = new Post(null, Instant.now(), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",  new AuthorDTO(maria));
+		Post post2 = new Post(null,Instant.now(), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
